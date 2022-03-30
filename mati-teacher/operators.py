baa7195eea0c803d1c2f981b4d3111c8ddf3937b
kacperwojcik4 <kacpernajtvi112357@gmail.com>
2022-03-30 22:23:33 +0200

    mati

---
+++ mati-teacher/operators.py
@@ -0,0 +1,32 @@
+a = 5
+b = 2
+
+print(a + b)  # 7
+print(a - b)  # 3
+print(a * b)  # 10
+print(a / b)  # 2.5
+print(a // b)  # 2
+print(a ** b)  # 25
+print(a % b)  # 1
+
+
+# strings
+print("kacper " + "wojcik")
+print("br" + 8 * "r")
+
+# boolean
+boolean = True
+
+print(boolean == True)
+print(boolean == False)
+
+print(boolean != True) # not equal =
+print(boolean != False) #
+
+count = 5
+
+print(count > 6)
+print(count >= 6)
+print(count < 6)
+print(count <= 6)
+
